581d6e6d2ea417a3911e52392a1b2bb711053b12
DAQEM <66999025+DAQEM@users.noreply.github.com>
2026-01-27 19:55:10 +0100

    fix: use condition hook

--- src/hooks/use-condition.ts
+++ src/hooks/use-condition.ts
@@ -3,16 +3,16 @@ import { conditionTypes, ConditionTypesKeys } from "@site/docs/arc/data";
 
 const useCondition = () => {
     const [_, __, category, id] = useDoc().metadata.id.split("/");
-    
-        var newId = id;
-        if (category === "jobsplus") {
-            newId = `jobsplus:${id}`;
-        } else {
-            newId = `arc:${id}`;
-        }
-        
-        const condition = conditionTypes[newId as ConditionTypesKeys];
-        return condition;
+
+    var newId = id !== undefined ? id : category;
+    if (category === "jobsplus") {
+        newId = `jobsplus:${newId}`;
+    } else {
+        newId = `arc:${newId}`;
+    }
+
+    const condition = conditionTypes[newId as ConditionTypesKeys];
+    return condition;
 };
 
 export default useCondition;
